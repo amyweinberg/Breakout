4aa752b255fa45cd9db12001d3ddade10d2ff127
Amy Weinberg <amyweinberg@gmail.com>
2018-06-01 13:28:06 -0400

    breakout board created with jquery

--- breakout.js
+++ breakout.js
@@ -1,18 +1,22 @@
 let init = $(function() {
-    console.log('jquery running');
+    console.log('jquery');
 });
 
-
-let container = $('#container');
-
-container.append('<div></div>').addClass('box-item');
+const numColumns = 10;
+const numRows = 5;
 
 
+let createDivs = function() {
+    
+    for(let x=1; x<=numRows; x++) {
+        for(let i=1; i<=numColumns; i++) {
+            let newDiv = $('<div></div>').insertBefore('.ball').addClass('box-item');
+            newDiv.addClass('col' + i + ' ' + 'row' + x);
+        }
+    }
+};
 
-
-
-
-
+createDivs();
 
 init;
 
